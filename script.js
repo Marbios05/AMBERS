2e5ec43f4b1913b3b50567907755912242c6f35f
Marbios05 <marbios.oficial@gmail.com>
2024-07-24 00:19:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 function startAdventure() {
-    console.log("startAdventure called");
+    console.log("startAdventure called"); // Para depuración
     document.querySelector('.welcome').style.display = 'none';
-    const treasureHunt = document.getElementById('treasure-hunt');
-    treasureHunt.style.display = 'block';
+    document.getElementById('treasure-hunt').style.display = 'block';
 
-    // Desplazarse automáticamente hacia la sección de búsqueda del tesoro
-    treasureHunt.scrollIntoView({ behavior: 'smooth' });
-}
-
-function revealMessage(id) {
-    console.log("revealMessage called with id:", id);
-    const message = document.getElementById(id);
-    message.classList.toggle('hidden');
-
-    // Desplazarse automáticamente hacia el mensaje revelado
-    message.scrollIntoView({ behavior: 'smooth' });
-}
-// Reproducir el audio
     const audio = document.getElementById('birthday-song');
-    audio.volume = 0; // Volumen inicial bajo
+
+    // Reproducir el audio con volumen inicial bajo
     audio.play().then(() => {
+        audio.volume = 0; // Volumen inicial bajo
         // Aumentar el volumen gradualmente
         const fadeInAudio = setInterval(() => {
             if (audio.volume < 1.0) {
